d7d7aafb67992fe9a5c7ae1116c3cfd6cfc4cb56
Aditya <wpsadi@outlook.com>
2024-12-06 21:16:22 +0000

    minor fixes in login #1

--- src/app/(pages)/(auth)/login/loginForm.tsx
+++ src/app/(pages)/(auth)/login/loginForm.tsx
@@ -56,7 +56,7 @@ export function LoginForm() {
   }
 
   return (
-    <Card className="shadow-lg bg-neutral-50   rounded-lg   max-w-[450px] min-w-[350px]   ">
+    <Card className="shadow-lg bg-neutral-50   rounded-lg   max-w-[400px] min-w-[350px]   ">
       <CardHeader>
         <CardTitle>Login</CardTitle>
         <CardDescription>
--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -6,14 +6,19 @@ import { LoginForm } from "./loginForm";
 function Page() {
   return (
     <>
-    <div className="min-h-screen flex items-center justify-center bg-neutral-50 lg:bg-gray-100">
-      <div className="grid gap-8 sm:place-content-center lg:grid-cols-2 mx-auto lg:container px-4 py-4">
-        <FormBrand />
-        <div className="flex items-center justify-center">
-        <LoginForm />
+      <section className="bg-gray-50 min-h-screen w-full flex items-center">
+        <div className="container mx-auto px-4 lg:px-8 py-8 lg:py-16 grid lg:grid-cols-2 items-center gap-8 lg:gap-16">
+          {/* Left Section */}
+          <div className="flex flex-col justify-center text-center lg:text-left">
+            <FormBrand />
+          </div>
+
+          {/* Right Section */}
+          <div className="w-full max-w-md mx-auto flex items-center justify-center lg:max-w-lg p-6 sm:p-8 rounded-lg shadow-lg">
+            <LoginForm />
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
     </>
   );
 }
