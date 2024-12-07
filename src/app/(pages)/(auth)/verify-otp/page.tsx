fe363bcc37bd38d84153913b11aa3829dc3b16a8
Aditya <wpsadi@outlook.com>
2024-12-07 17:36:12 +0000

    Shit just got turned into gold

--- src/app/(pages)/(auth)/verify-otp/page.tsx
+++ src/app/(pages)/(auth)/verify-otp/page.tsx
@@ -1,25 +1,17 @@
 "use client";
-import { useSearchParams } from "next/navigation";
+
 import { Suspense } from "react";
 
 import { ErrorPage } from "@/components/common/errorRaiser";
 
-import { OTPVerificationForm } from "./VerifyOTPForm";
+import VerifyOTPBordered from "./BoundaryWrapperVerifyOTP";
 
 // it get email & otp_key from the query params
 export default function Page() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
-  const otpKey = searchParams.get("otp_key");
-
-  if (!email || !otpKey) {
-    return <ErrorPage message="Missing email or OTP key" />;
-  }
-
   return (
     <>
       <Suspense fallback={<ErrorPage message="Unable to render the page" />}>
-        <OTPVerificationForm email={email} otpKey={otpKey} />
+        <VerifyOTPBordered />
       </Suspense>
     </>
   );
